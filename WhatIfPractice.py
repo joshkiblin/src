914b031effff61270dea2b1d9524653c97845af5
Nathaniel <ntomtishen@shortridgeacademy.com>
2016-02-16 10:08:29 -0500

    Updated WhatIf

--- WhatIfPractice.py
+++ WhatIfPractice.py
@@ -1,3 +1,14 @@
-age = 18
+age = '18'
+convertedAge = int(age)
 if age > 20:
 	print('You are too old!')
+else:
+	print('You are too young!')
+
+numberOfLimbs = 16
+if numberOfLimbs == 4:
+	print('Congrats! You have all of your limbs')
+elif numberOfLimbs > 4:
+	print('Are you an alien?')
+elif numberOfLimbs < 4:
+	print('Unlucky')

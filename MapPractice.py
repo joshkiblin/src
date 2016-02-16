7ec7b1d77898e2d9960f96d0da58636fc6ad2998
Nathaniel <ntomtishen@shortridgeacademy.com>
2016-02-16 09:47:48 -0500

    Adding Map, Turtle, WhatIf

---
+++ MapPractice.py
@@ -0,0 +1,16 @@
+school = {'Nathaniel' : 'Shortridge',
+				'Kiernan' : 'Community',
+				'Rhiannon' : 'Stanford',
+				'Meeko' : 'Life'}
+
+print(school['Nathaniel'])
+
+print(school)
+
+del school['Meeko']
+
+print(school)
+
+school['Nathaniel'] = 'Shortridge Academy'
+
+print(school)
---
+++ TurtlePractice.py
@@ -0,0 +1,18 @@
+import turtle
+t = turtle.Pen()
+
+t.forward(150)
+t.left(90)
+t.forward(150)
+t.left(90)
+t.forward(150)
+t.left(90)
+t.forward(150)
+t.reset()
+t.backward(100)
+t.up()
+t.right(90)
+t.forward(20)
+t.left(90)
+t.down
+t.forward(150)

d428a1d2e7057019c74a54b607f5c6922e808ff0
Nathaniel <ntomtishen@shortridgeacademy.com>
2016-02-18 09:38:19 -0500

    Added Five.py

---
+++ Five.py
@@ -0,0 +1,11 @@
+number = 5
+
+print (number)
+
+print (number * number)
+
+print (number * number * number)
+
+number2 = number * 5555555
+
+print (number2)
---
+++ NamePrint.py
@@ -0,0 +1,3 @@
+name = "Nathaniel"
+
+print("My name is " + name + "! I like to DANCE!")

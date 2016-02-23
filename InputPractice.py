d0ba57c18c04b9430428a556f186404514e7d816
Nathaniel <ntomtishen@shortridgeacademy.com>
2016-02-23 10:08:57 -0500

    Modified InputPractice.py

--- InputPractice.py
+++ InputPractice.py
@@ -1,5 +1,20 @@
+# Get car A name
 a = raw_input('Please enter the name of the hybrid car: ')
-print(a)
 
+# Get car B name
+b = raw_input('Please enter the name of the non-hybrid car: ')
+
+#Get car A price
 pa = input('Please enter the price of the hybrid car: ')
-print(pa)
+
+#Get car B price
+pb = input('Please enter the price of the non-hybrid car: ')
+
+# Subtract price of car b from car a to get the retail cost difference
+RCD = pa - pb
+
+gp = input('Please enter the average price of gasoline per gallon for your area: ')
+
+mpgA = input('Please enter the combined mpg for %s: ' % (a))
+
+mpgA = input('Please enter the combined mpg for %s: ' % (b))

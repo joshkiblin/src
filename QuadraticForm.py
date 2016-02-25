31e3307fa05ebaa6b45aab583755af50f463c3ad
Nathaniel <ntomtishen@shortridgeacademy.com>
2016-02-24 20:18:35 -0500

    Created QuadraticForm.py

---
+++ QuadraticForm.py
@@ -0,0 +1,9 @@
+import math
+
+a = raw_input("What is the x^2 value: ")
+b = raw_input("What is the x value: ")
+c = raw_input("What is the c value: ")
+
+answerOne = ((-b + (math.sqrt(math.pow(b, 2) - (4 * a * c)))/(2 * a))
+
+print(answerOne)

deee92f941399efd877ffc4d7ce77a7b08b07510
Nathaniel <ntomtishen@shortridgeacademy.com>
2016-02-26 10:09:53 -0500

    Created AtomicEmission.py

---
+++ AtomicEmission.py
@@ -0,0 +1,8 @@
+
+print('')
+print('Welcome to the Amazing Atomic Emission application!')
+print('This program will use the E value you measured and recorded in')
+print('scientific notation in order to determine the relative energy of')
+print('the energy level transition along with reporting the wavelength')
+print('in meters and frequency in Hertz of the energy emission.')
+print('')

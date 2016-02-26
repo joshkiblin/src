a9f7ec13b3768767f2e868ce0cee5fc9c33d5315
Nathaniel <ntomtishen@shortridgeacademy.com>
2016-02-26 09:12:48 -0500

    Modified ClassesPractice.py

--- ClassesPractice.py
+++ ClassesPractice.py
@@ -23,11 +23,19 @@ class Mammals(Animals):
 		print ('feeding young')
 
 class Giraffes(Mammals):
+	def __init__(self, spots):
+		self.giraffes_spots = spots
 	def eat_leaves(self):
 		print('eating leaves')
+	def find_food(self):
+		self.move()
+		print("I've found food!")
+		self.eat_food()
 
-reginald = Giraffes()
+reginald = Giraffes(123)
 
 reginald.move()
 reginald.eat_leaves()
 reginald.breathe()
+reginald.find_food()
+print(reginald.giraffes_spots)

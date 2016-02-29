09cf6248d46484ed5fac8c33c9ab37235a7a5a5a
Nathaniel <ntomtishen@shortridgeacademy.com>
2016-02-29 10:59:43 -0500

    Modified RandomPractice.py

--- RandomPractice.py
+++ RandomPractice.py
@@ -1,2 +1,17 @@
 import random
-print(random.randint(1, 100))
+num = random.randint(1, 100)
+print(num)
+
+while True:
+	print('Guess a number between 1 and 100')
+	guess = input()
+	i = int(guess)
+	if i > 100 or i < 1:
+		print("You guessed a number that wasn't within the range")
+	elif i == num:
+		print("You're right!")
+		break
+	elif i > num:
+		print('Too high')
+	elif i < num:
+		print('Too low')

90c140c577fcba314ba7e7f2360edde0eb2bd4b2
Nathaniel <ntomtishen@shortridgeacademy.com>
2016-03-02 18:53:19 -0500

    Modified Bounce.py and created Ball.py

---
+++ Ball.py
@@ -0,0 +1,13 @@
+import Tkinter
+import random
+import time
+
+class Ball:
+	def __init__(self, canvas, color):
+		self.canvas = canvas
+		self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
+		self.canvas.move(self.id, 245, 100)
+
+	def draw(self):
+		pass
+		
--- Bounce.py
+++ Bounce.py
@@ -1,12 +1,21 @@
+# Imports various modules we need
 import Tkinter
 import random
 import time
+from Ball import Ball
 
+# Creates a Tkinter object
 top = Tkinter.Tk()
+# Gives the window the title of 'Game'
 top.title('Game')
+# Sets the window so that it isn't resizable
 top.resizable(0, 0)
+# Places the window on top of all other windows
 top.wm_attributes("-topmost", 1)
+
 canvas = Tkinter.Canvas(top, width=500, height=400, bd=0, highlightthickness=0)
 canvas.pack()
 top.update()
-top.mainloop()
+
+ball = Ball(canvas, 'red')
+canvas.mainloop()

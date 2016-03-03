ea6a6a0b06c27c8587d0a590d946ca4db9684248
Nathaniel <ntomtishen@shortridgeacademy.com>
2016-03-03 09:25:43 -0500

    Added the ability for the paddle to move in Paddle.py

--- Paddle.py
+++ Paddle.py
@@ -18,9 +18,30 @@ class Paddle:
 		# Sets canvas_width to the current width of the canvas
 		self.canvas_width = self.canvas.winfo_width()
 
+		# Binds pressing the left arrow key to the turn_left function
+		self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+		# Binds pressing the right arrow key to the turn_right function
+		self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
+
 	# Creates a draw function for the Paddle class
 	def draw(self):
-		pass
+		# Calls the function move on the canvas
+		# Passes 3 parameters: the id of the rectangle, the x we set in the __init__ function
+		# We set 0 for the y since the paddle doesn't need to move up or down
+		self.canvas.move(self.id, self.x, 0)
+	 	# Creates a variable called pos by calling the canvas function coords
+		# coords returns the curent x and y coordinates of the parameter you pass it (self.id here)
+		# Returns the position in the format [x1, y1, x2, y2] (Top left, top right)
+		pos = self.canvas.coords(self.id)
+		# Checks to see if the x1 value is equal to 0. If it is then the paddle has hit the left side
+		if pos[0] <= 0:
+			# If it has then we set x to 0
+			self.x = 0
+		# Checks to see if the x2 value is equal to the width of the canvas. If it is then the paddle
+		# has hit the right side of the screen
+		elif pos[2] >= self.canvas_width:
+			# If it has then we set x to 0
+			self.x = 0
 
 	# A function to turn the paddle left
 	def turn_left(self, evt):

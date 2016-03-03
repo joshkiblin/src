ffcea36f543a6ebdc24fc2aacb05bd5f65bf13c8
Nathaniel <ntomtishen@shortridgeacademy.com>
2016-03-02 20:22:13 -0500

    Added the turn_left and the turn_right functions to Paddle.py

--- Paddle.py
+++ Paddle.py
@@ -13,7 +13,21 @@ class Paddle:
 		self.id = canvas.create_rectangle(0, 0, 100, 10, fill=color)
 		# Moves the rectangle to the middle of the canvas
 		self.canvas.move(self.id, 200, 300)
+		# Sets x to 0
+		self.x = 0
+		# Sets canvas_width to the current width of the canvas
+		self.canvas_width = self.canvas.winfo_width()
 
 	# Creates a draw function for the Paddle class
 	def draw(self):
 		pass
+
+	# A function to turn the paddle left
+	def turn_left(self, evt):
+		# Sets x to -2
+		self.x = -2
+
+	# A function to turn the paddle right
+	def turn_right(self, evt):
+		# Sets x to 2
+		self.x = 2

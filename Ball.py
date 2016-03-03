3ea67bdc1194d00e427d36be9c7ae884d9c5340e
Nathaniel <ntomtishen@shortridgeacademy.com>
2016-03-02 20:08:03 -0500

    Modified Ball.py

--- Ball.py
+++ Ball.py
@@ -13,12 +13,20 @@ class Ball:
 		self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
 		# Moves the oval to the middle of the canvas
 		self.canvas.move(self.id, 245, 100)
-		# Sets x to 0 for the object
-		self.x = 0
-		# Sets y to -1 for the object
-		self.y = -1
+		
+		# Creates a list of 6 potential starts
+		starts = [-3, -2, -1, 1, 2, 3]
+		# Uses random to shuffle to order in starts
+		random.shuffle(starts)
+		# Chooses the new first item in starts
+		self.x = starts[0]
+		# Selts y to -3
+		self.y = -3
+		
 		# Set the variable canvas_height to the current height of the canvas
 		self.canvas_height = self.canvas.winfo_height()
+		# Set the variable canvas_width to the current width of the canvas
+		self.canvas_width = self.canvas.winfo_width()
 
 	# Creates a draw function for the Ball class
 	def draw(self):
@@ -27,16 +35,29 @@ class Ball:
 		self.canvas.move(self.id, self.x, self.y)
 		# Creates a variable called pos by calling the canvas function coords
 		# coords returns the curent x and y coordinates of the parameter you pass it (self.id here)
-		# Returns the position in the format [x1, y1, x2, x2] (Top left, top right)
+		# Returns the position in the format [x1, y1, x2, y2] (Top left, top right)
 		pos = self.canvas.coords(self.id)
-		# Checks to see if y1 (top of ball) is less then or equal to 0
+		# Checks to see if y1 (top of ball) is less than or equal to 0
+		# This would mean the bottom of the ball has hit the top of the screen
 		if pos[1] <= 0:
-			# If it is then we set y to 1. This stops the ball from moving up 
-			# if it hits the top of the screen and it moves down instead
-			self.y = 1
-		# Checks to see if y2 is greater than or equal to canvas_height
+			# If it has then we set y to 1. This stops the ball from moving up 
+			# now that it has hit the top of the screen and it moves down instead
+			self.y = 3
+		# Checks to see if y2 (bottom of ball) is greater than or equal to canvas_height
+		# This would mean the the bottom of the ball has hit the bottom of the screen
 		if pos[3] >= self.canvas_height:
-			# If it is then we set y to -1. This stops the ball from moving down
-			# if it hits the bottom of the screen and it moves up instead
-			self.y = -1
-		
+			# If it has then we set y to -1. This stops the ball from moving down
+			# now that it has hit the bottom of the screen and it moves up instead
+			self.y = -3
+		# Checks to see if x1 (top left of ball) is less than or equal to 0
+		# This would mean that the left side of the ball has hit the left side of the screen
+		if pos[0] <= 0:
+			# If it has then we set x to 3 which would now make it move right. This stops the ball
+			# from moving left now that it has hit the left side of the screen.
+			self.x = 3
+		# Checks to see if x2 (the bottom right of the ball) is greater than or equal to canvas_width
+		# This would mean that the right side of the ball has hit the right side of the screen
+		if pos[2] >= self.canvas_width:
+			# If it has then we set x to -3 which would now make it move left. This stops the ball
+			# from moving right now that it has hit the right side of the screen.
+			self.x = -3

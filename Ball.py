8cc78a24d06046f165fe67c22f18f2a7216edfe1
Nathaniel <ntomtishen@shortridgeacademy.com>
2016-03-02 19:01:23 -0500

    Modified Ball.py and Bounce.py

--- Ball.py
+++ Ball.py
@@ -1,11 +1,17 @@
+# Import modules that we need
 import Tkinter
 import random
 import time
 
+#Creates the class 'Ball'
 class Ball:
+	# An initialization function. User pass two parameters: 'canvas' and 'color'
 	def __init__(self, canvas, color):
+		# Set the object canvas to the parameter 'canvas' that the user passed
 		self.canvas = canvas
+		# Creates an oval. Uses the color that the user passed
 		self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
+		# Moves the oval to the middle of the canvas
 		self.canvas.move(self.id, 245, 100)
 
 	def draw(self):
--- Bounce.py
+++ Bounce.py
@@ -13,9 +13,15 @@ top.resizable(0, 0)
 # Places the window on top of all other windows
 top.wm_attributes("-topmost", 1)
 
+# Creates the canvas and passes it several parameters:
+# bd = 0 and highlightthickness = 0 make sure that there isn't a border around the screen
 canvas = Tkinter.Canvas(top, width=500, height=400, bd=0, highlightthickness=0)
+# Tells the canvas to size itself according to the width and height parameters we pass in line 17
 canvas.pack()
+# Tells Tkinter to initialize itself
 top.update()
 
+# Creates an object named 'ball' of the Ball class that we created in Ball.py
 ball = Ball(canvas, 'red')
+# Tells the canvas to not close until the user close the window
 canvas.mainloop()
